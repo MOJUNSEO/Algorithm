d1f422221ee0180de41764faf84c70c7ed2a499b
DESKTOP-K83AFO8\wisdomose <mojunseo49@naver.com>
2018-03-24 02:04:16 +0900

    window_update

--- src/Problems/noXXXX_warmingup_forMaestro.java
+++ src/Problems/noXXXX_warmingup_forMaestro.java
@@ -1,5 +1,9 @@
 package Problems;
 
-public class noXXXX_warmingup_forMaestro {
+import Last_Algorithm.Main;
 
+public class noXXXX_warmingup_forMaestro {
+	public static void main(String[] args) {
+		
+	}
 }
