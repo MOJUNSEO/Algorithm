3435deceb2a0aeb57fae7585e4847f1079297929
MOJUNSEO <mojunseo49@naver.com>
2018-07-22 21:08:02 +0900

    commit

--- src/Problems/no14503_RobotCleaner_DFSorBFS.java
+++ src/Problems/no14503_RobotCleaner_DFSorBFS.java
@@ -6,16 +6,22 @@ import java.util.Scanner;
 
 /*
  * https://www.acmicpc.net/problem/14503
+ * 
  * */
-public class no14503_RobotCleaner_DFSorBFS {
+public class no14503_RobotCleaner_DFSorBFS 
+{
 	static int N = 0;
 	static int M = 0;
 	static int map[][];
-	static int visit[][];
-	//북,서,남,동
+	static boolean visit[][];
+	//북,동,남,서
 	static int way[];
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	static int mask_c[] = {-1,0,1,0};
+	static int mask_r[] = {0,1,0,-1};
+	static int count=0;
+	public static void main(String[] args) 
+	{
+ 		Scanner sc = new Scanner(System.in);
 		
 		N = sc.nextInt();
 		M = sc.nextInt();
@@ -27,6 +33,7 @@ public class no14503_RobotCleaner_DFSorBFS {
 		
 		
 		map = new int[N][M];
+		visit = new boolean[N][M];
 		
 		for(int i=0;i<N;i++)
 		{
@@ -35,152 +42,156 @@ public class no14503_RobotCleaner_DFSorBFS {
 				map[i][j] = sc.nextInt();
 			}
 		}
-		//북,서,남,동
-		switch(direction)
-		{
-		 case 0 :
-			 //북
-			 way[0] = 1;
-			 break;
-		 case 1 :
-			 //동
-			 way[3] = 1;
-			 break;
-		 case 2 :
-			 //남
-			 way[2] = 1;
-			 break;
-		 case 3 :
-			 //서
-			 way[1] = 1;
-			 break;
-			
-		}
-		
+
 		Bfs(Rcol,Rrow,direction);
-		
-		
+		System.out.println(count);
+	}
+	
+	private static void Countsection()
+	{
+		int count = 0;
+		for(int i=0;i<N;i++)
+		{
+			for(int j=0;j<M;j++)
+			{
+				if(visit[i][j] == true)
+				{
+					count++;
+				}
+			}
+		}
+		System.out.println(count);
 	}
 	
 	//청소를 하면 1로 만듬 
 	public static void Bfs(int startcol, int startrow, int direction)
 	{
-		int maskcol = {};
 		Queue<robot> q = new LinkedList<>();		
 		q.add(new robot(startcol, startrow, direction));
 		
-		visit[startcol][startrow] = 1;
+		++count;
+		visit[startcol][startrow] = true;
 		
 		while(!q.isEmpty())
 		{
 			int ncol = q.peek().col;
 			int nrow = q.peek().row;
 			int ndi = q.poll().di;
+			boolean noanyway = true;
 			//갈수 잇는곳이 잇으면 
-			if(check4way(ncol,nrow) == true)
+			for(int i=0;i<4;i++)
 			{
-				//왼쪽방향 탐색해야함
-				int nextdi = changeleft(ndi);
-				
-				//방향을 가지고 발견될때까지 반복해야할듯 
+				//현재 방향이 북쪽 이면 다음탐색은 서쪽  
+				//지금 방향이 서쪽이면 다음은 남쪽 
+				//지금 방향이 남쪽이면 동쪽 
+				//지금 방향이 동쪽이면 북쪽
+				int nextdi = nextsearchway(ndi);
+				int nextcol = ncol + mask_c[nextdi];
+				int nextrow = nrow + mask_r[nextdi];
+				if(nextcol > -1 && nextcol < N)
+				{
+					if(nextrow > -1 && nextrow < M)
+					{
+						if(map[nextcol][nextrow] == 0 && visit[nextcol][nextrow] == false)
+						{
+							visit[nextcol][nextrow] = true;
+							++count;
+							q.add(new robot(nextcol, nextrow, nextdi));
+							noanyway = false;
+							break;
+						}
+					}
+				}
 			}
-			else
+			if(noanyway == true)
 			{
-				//뒤방향 확인해서 후진못하면 끝
-				//후진 할 수 있으면 후진좌표 add
+				int backdi = backdirection(ndi);
+				int backcol = ncol + mask_c[backdi];
+				int backrow = nrow + mask_r[backdi];
+				if(backcol > -1 && backcol < N && backrow > 0 &&backrow < M)
+				{
+					if(map[backcol][backrow] == 0)
+					{
+						q.add(new robot(backcol,backrow,ndi));
+					}
+				}
+				//현재 방향에서 북이면 남, 
+				//남이면 북 
+				//동이면 서 
+				//서면 동 으로 2칸.
 			}
 			
 		}
 	}
 	
-	private static boolean check4way(int nowcol, int nowrow)
+	private static int backdirection(int direction)
 	{
-		//좌,상,하,우
-		int maskcol[] = {0,-1,1,0};
-		int maskrow[] = {-1,0,0,1};
-		int ncol = nowcol;
-		int nrow = nowrow;
-		boolean result = true;
-		for(int i=0;i<4;i++)
+		int returnway = -1;
+		//북 - > 남 
+		//북,동,남,서
+		if(direction == 0)
 		{
-			int nextcol = ncol + maskcol[i];
-			int nextrow = nrow + maskrow[i];
-			//갈 수 있으면 true
-			if(nextcol > -1 && nextcol < N && nextrow > -1 && nextrow < M)
-			{
-				//벽이 아니고
-				if(map[nextcol][nextrow] != 1)
-				{
-					//아직 방문안했으면
-					if(visit[nextcol][nextrow] != 1)
-					{
-						return result;
-					}
-				}
-				else
-				{
-					result = false;
-					return result;
-				}
-			}
-			else
-			{
-				result = false;
-				return result;
-			}
+			returnway = 2;
 		}
-		return result;
-	}
-	
-	//바라보는 방향에서 왼쪽으로 방향바꿔줌 
-	private static int changeleft(int direction)
-	{
-		//북,서,남,동
-		int returnway = 0;
-		if(way[direction] == 1)
+		//남- >  북 
+		else if(direction == 1)
 		{
-			//북 - >서
-			if(direction == 0)
-			{
-				way[direction] = 0;
-				way[1] = 1;
-				returnway = 1;
-			}
-			// 동 -> 북
-			else if(direction == 1)
-			{
-				way[direction] = 0;
-				way[0] = 1;
-				returnway = 0;
-			}
-			// 남 -> 동
-			else if(direction == 2)
-			{
-				way[direction] = 0;
-				way[3] = 1;
-				returnway = 3;
-			}
-			// 서 -> 남
-			else if(direction == 3)
-			{
-				way[direction] = 0;
-				way[2] = 1;
-				returnway = 2;
-			}
+			returnway = 3;
+		}
+		//동 - > 서  
+		else if(direction == 2)
+		{
+			returnway = 0;
+		}
+		//서  - > 동  
+		else if(direction == 3)
+		{
+			returnway = 1;	
 		}
 		return returnway;
 	}
-}
-
-class robot
-{
-	int col;
-	int row;
-	int di;
-	public robot(int icol, int irow, int idi)
+	
+	//바라보는 방향에서 왼쪽으로 방향바꿔줌 
+	private static int nextsearchway(int direction)
 	{
-		this.col = icol;
-		this.row = irow;
-		this.di = idi;
+		//북,동,남,서
+		//현재 방향이 북쪽 이면 다음탐색은 서쪽  
+		//지금 방향이 서쪽이면 다음은 남쪽 
+		//지금 방향이 남쪽이면 동쪽 
+		//지금 방향이 동쪽이면 북쪽 
+		if(direction == 0)
+		{
+			return 3; 
+		}
+		else if(direction == 1)
+		{
+			return 0;
+		}
+		else if(direction == 2)
+		{
+			return 1;
+		}
+		else if(direction == 3)
+		{
+			return 2;
+		}
+		return -1;
 	}
 }
+	class robot
+	{
+		int col;
+		int row;
+		int di;
+		public robot(int icol, int irow, int idi)
+		{
+			this.col = icol;
+			this.row = irow;
+			this.di = idi;
+		}
+	}
+
+
+
+
+
